f8ff9ca9e272b0517ee66f591c2702148092e894
Jan Rochel <jan.rochel@cea.fr>
2026-07-22 17:18:52 +0200

    add --libc-replacements and full memory tracking
    This helps catching problematic utilisation of libc functions.

--- PrintBBUIDs/build.sh
+++ PrintBBUIDs/build.sh
@@ -10,7 +10,7 @@ INPUT_FILE_OBJ="input/$1.o"
 INPUT_FILE_EXE="input/$1.exe"
 
 # the following variable has been obtained by running:
-# e-acsl-gcc $INPUT_FILE
+# e-acsl-gcc --libc-replacements $INPUT_FILE
 EACSL_LINKING_FLAGS="-DE_ACSL_SEGMENT_MMODEL -std=c99 -m64 -g -O2 -fno-builtin -fno-merge-constants -Wall -Wno-long-long -Wno-attributes -Wno-nonnull -Wno-undef -Wno-unused -Wno-unused-function -Wno-unused-result -Wno-unused-value -Wno-unused-function -Wno-unused-variable -Wno-unused-but-set-variable -Wno-implicit-function-declaration -Wno-empty-body -I/home/opam/.opam/4.14/share/frama-c/share/e-acsl /home/opam/.opam/4.14/share/frama-c/share/e-acsl/e_acsl_rtl.c /home/opam/.opam/4.14/lib/frama-c-e-acsl/libeacsl-dlmalloc.a -lgmp -lm"
 
 if [ "$#" -eq 1 ] && [[ -f $INPUT_FILE ]]; then
@@ -19,10 +19,10 @@ if [ "$#" -eq 1 ] && [[ -f $INPUT_FILE ]]; then
   . /home/opam/.opam/opam-init/init.sh
 
   # the following command has been obtained by running:
-  # e-acsl-gcc $INPUT_FILE
-  frama-c -keep-unused-functions none -machdep gcc_x86_64 '-cpp-extra-args= -std=c99 -D_DEFAULT_SOURCE -D__NO_CTYPE -D__FC_MACHDEP_X86_64 ' $INPUT_FILE -rte -e-acsl -e-acsl-share=/home/opam/.opam/4.14/share/frama-c/share/e-acsl -no-frama-c-stdlib -then-last -print -ocode $INPUT_FILE_EACSL
+  # e-acsl-gcc --libc-replacements $INPUT_FILE
+  frama-c -keep-unused-functions none -machdep gcc_x86_64 '-cpp-extra-args= -std=c99 -D_DEFAULT_SOURCE -D__NO_CTYPE -D__FC_MACHDEP_X86_64 ' $INPUT_FILE -rte -e-acsl -e-acsl-full-mtracking -e-acsl-replace-libc-functions -e-acsl-share=/home/opam/.opam/4.14/share/frama-c/share/e-acsl -no-frama-c-stdlib -then-last -print -ocode $INPUT_FILE_EACSL
   
-  $LLVM_DIR/bin/clang -O0 -emit-llvm -I/home/opam/.opam/4.14/share/frama-c/share/e-acsl $INPUT_FILE_EACSL -c -o $INPUT_FILE_BC
+  $LLVM_DIR/bin/clang -O0 -emit-llvm -I/home/opam/.opam/4.14/share/frama-c/share/e-acsl -include libc_replacements/e_acsl_string.h $INPUT_FILE_EACSL -c -o $INPUT_FILE_BC
 
   $LLVM_DIR/bin/opt -load-pass-plugin $PASS_DIR/build/libPrintBBUIDs.so --passes="print-bb-uids" $INPUT_FILE_BC -o $INPUT_FILE_BCI
   
